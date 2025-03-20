b0b8c02d8380215f24b6a9e3bab9adcd582ab0fe
sh4dow <shadoxxhd@users.noreply.github.com>
2025-03-20 05:46:14 +0100

    improved replacement logic for locomotives and wagons

--- control.lua
+++ control.lua
@@ -1,6 +1,173 @@
 local name_lookup = nil
 local qname = nil
 
+-- SECTION code taken from train-upgrader mod
+local carriage_inventories = {
+  defines.inventory.fuel,
+  defines.inventory.burnt_result,
+  defines.inventory.cargo_wagon,
+  defines.inventory.artillery_wagon_ammo
+}
+
+
+local function car_inv(car, typ)
+  local inv = car.get_inventory(typ)
+  if ((inv == nil) or (not inv.valid)) then return nil end
+  return inv
+end
+
+function record_inventory(car)
+  local contents = nil
+  for _, inv_type in pairs(carriage_inventories) do
+    local inv = car_inv(car, inv_type)
+    if (inv ~= nil) then
+      local entry = { slots = #inv }
+      if (inv.supports_bar()) then
+        entry.bar = inv.get_bar()
+        if (entry.bar > entry.slots) then
+          entry.bar = nil
+        else
+          entry.slots = entry.bar
+        end
+      end
+      if (inv.is_filtered()) then
+        entry.filters = { }
+        for i = 1, entry.slots do
+          entry.filters[i] = inv.get_filter(i)
+        end
+      end
+      if ((not inv.is_empty()) or (entry.bar ~= nil) or
+          (entry.filters ~= nil)) then
+        if (contents == nil) then contents = { } end
+        entry.contents = inv.get_contents()
+        contents[inv_type] = entry
+      end
+    end
+  end
+  return contents
+end
+
+function restore_inventory(car, contents)
+  if (contents == nil) then return end
+  for inv_type, entry in pairs(contents) do
+    local inv = car.get_inventory(inv_type)
+    if ((inv ~= nil) and inv.valid) then
+      local slots = #inv
+      if (inv.supports_bar() and (entry.bar ~= nil) and
+          (entry.bar < slots)) then
+        inv.set_bar(entry.bar)
+        slots = entry.bar
+      end
+
+      if (inv.supports_filters() and (entry.filters ~= nil)) then
+        if (slots == entry.slots) then
+          for i = 1, entry.slots do
+            inv.set_filter(i, entry.filters[i])
+          end
+        else
+          local totals = { }
+          local different = 0
+          for i = 1, entry.slots do
+            local filter = entry.filters[i]
+            if (filter == nil) then filter = " " end
+            if (totals[filter] == nil) then
+              totals[filter] = 1
+              different = different + 1
+            else
+              totals[filter] = totals[filter] + 1
+            end
+          end
+          
+          local oldslots = entry.slots
+          local newslots = slots
+          local current = 1
+          for filter, count in pairs(totals) do
+            local num = 1
+            if (oldslots > 0) then
+              local fraction = (newslots * count) / oldslots
+              num = math.max(1, math.floor(fraction + 0.5))
+            end
+            local last = current + num - 1
+            if (last > slots) then
+              last = slots
+            end
+            if ((filter ~= " ") and (last >= current)) then
+              for i = current, last do
+                inv.set_filter(i, filter)
+              end
+            end
+            current = last + 1
+            oldslots = oldslots - count
+            newslots = newslots - num
+          end
+        end
+      end
+
+      for itemname, amount in pairs(entry.contents) do
+        local inserted = inv.insert({name=itemname, count=amount})
+        local remaining = amount - inserted
+        if (remaining < 1) then remaining = nil end
+        entry.contents[itemname] = remaining
+      end
+    end
+  end
+end
+
+function record_grid(grid)
+  if ((grid == nil) or (not grid.valid)) then return nil end
+  if (grid.count() < 1) then return nil end
+  local contents = { }
+  local num = 0
+  for _, eq in pairs(grid.equipment) do
+    if (eq.valid) then
+      num = num + 1
+      local entry = { name = eq.name,
+          position = eq.position, proto = eq.prototype}
+      if ((eq.burner ~= nil) and eq.burner.valid) then
+        if ((eq.burner.inventory ~= nil) and eq.burner.inventory.valid) then
+          entry.fuel = eq.burner.inventory.get_contents()
+        end
+        if ((eq.burner.burnt_result_inventory ~= nil) and
+            eq.burner.burnt_result_inventory.valid) then
+          entry.burnt = eq.burner.burnt_result_inventory.get_contents()
+        end
+      end
+      contents[num] = entry
+    end
+  end
+  return contents
+end
+
+function restore_grid(grid, contents)
+  if (contents == nil) then return false end
+  if ((grid == nil) or (not grid.valid)) then return false end
+  local ret = false
+  for _, eq in pairs(contents) do
+    local neweq = grid.put{ name = eq.name, position = eq.position }
+    if (neweq ~= nil) then
+      ret = true
+      if ((neweq.burner ~= nil) and neweq.burner.valid) then
+        local burn = neweq.burner
+        if ((eq.fuel ~= nil) and (burn.inventory ~= nil) and
+            burn.inventory.valid) then
+          for itemname, amount in pairs(eq.fuel) do
+            burn.inventory.insert({name=itemname, count=amount})
+          end
+        end
+        if ((eq.burnt ~= nil) and (burn.burnt_result_inventory ~= nil) and
+            burn.burnt_result_inventory.valid) then
+          for itemname, amount in pairs(eq.burnt) do
+            burn.burnt_result_inventory.insert({name=itemname, count=amount})
+          end
+        end
+      end
+    end
+  end
+  return ret
+end
+
+-- END SECTION
+
 local function check_entity(entity_name, qname)
     if name_lookup[entity_name] ~= nil then return name_lookup[entity_name] end
     name_lookup[entity_name] = prototypes.entity[qname.."-"..entity_name] and true or false
@@ -14,19 +181,53 @@ local on_built = function (data)
 
     local surface = entity.surface
     local info = {
-        name = entity.quality.name .. "-" .. entity.name,
-        position = entity.position,
-        quality = entity.quality,
-        force = entity.force,
-        fast_replace = true,
-        player = entity.last_user,
-        orientation = entity.orientation,
-        direction = entity.direction,
-        raise_built = true,
-        spill=false
-    }
-    --entity.destroy()
-    surface.create_entity(info)
+            name = entity.quality.name .. "-" .. entity.name,
+            position = entity.position,
+            quality = entity.quality,
+            force = entity.force,
+            fast_replace = true,
+            player = entity.last_user,
+            orientation = entity.orientation, -- rolling stock
+            color = entity.color, -- rolling stock
+            direction = entity.direction, -- buildings
+            raise_built = true,
+            spill=false
+        }
+    if entity.type == "locomotive" or entity.type == "cargo-wagon" or entity.type == "fluid-wagon" or entity.type == "artillery-wagon" then
+        -- mobile entities -> fast replace wont work
+        -- store relevant info (commented out: not relevant for newly placed entities)
+        if entity.train then
+            local sched = entity.train.schedule
+            local manual = entity.train.manual_mode
+        end
+        local inv = record_inventory(entity)
+        local grid = record_grid(entity.grid)
+        --local fluid = entity.type == "fluid-wagon" and entity.get_fluid_contents() or nil
+        --local burner = nil
+        --if entity.burner and entity.burner.valid then
+        --    burner = {entity.burner.currently_burning, entity.burner.remaining_burning_fuel}
+        --end
+
+        entity.destroy()
+        local ne = surface.create_entity(info)
+        -- restore properties
+        --if fluid then
+        --    for name, amount in pairs(fluid) do ne.insert_fluid({name=name,amount=amount}) end
+        --end
+        restore_inventory(ne, inv)
+        restore_grid(ne.grid, grid)
+        --if ne.burner and ne.burner.valid and ne.burner.fuel_categories[burner[1].fuel_category] then
+        --    ne.burner.currently_burning = burner[1]
+        --    ne.burner.remaining_burning_fuel = burner[2]
+        --end
+        if ne.train and manual_mode ~= nil then
+            ne.train.schedule = sched
+            ne.train.manual_mode = manual or false
+        end
+    else
+        --entity.destroy()
+        surface.create_entity(info)
+    end
 end
 
 -- todo: implement logic to optionally replace all existing quality buildings that are not affected yet
