ec26ee4d7883d165f51035b63aa0cba024fb306b
sh4dow <shadoxxhd@users.noreply.github.com>
2026-06-17 15:57:32 +0200

    typo fix

--- data.lua
+++ data.lua
@@ -1,5 +1,5 @@
 -- default blacklist
 if (not data.raw["mod-data"]) or (not data.raw["mod-data"]["clone-blacklist"]) then
-    data:extend({{type="mod-data", name="entity-clones", data={}}})
+    data:extend({{type="mod-data", name="clone-blacklist", data={}}})
 end
 data.raw["mod-data"]["clone-blacklist"].data["agricultural-roboport"] = true
