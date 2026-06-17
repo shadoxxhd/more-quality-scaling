6bc22a58b3eafb1532deeef035f5db878569d601
sh4dow <shadoxxhd@users.noreply.github.com>
2026-06-17 17:45:07 +0200

    added incompatible cargo-ships entities to default blacklist

--- data.lua
+++ data.lua
@@ -2,4 +2,11 @@
 if (not data.raw["mod-data"]) or (not data.raw["mod-data"]["clone-blacklist"]) then
     data:extend({{type="mod-data", name="clone-blacklist", data={}}})
 end
+
+-- agricultural roboport
 data.raw["mod-data"]["clone-blacklist"].data["agricultural-roboport"] = true
+
+-- cargo-ships
+for _,name in pairs({"cargo_ship", "boat", "boat_engine", "oil_tanker", "cargo_ship_engine", "oil_rig"}) do
+    data.raw["mod-data"]["clone-blacklist"].data[name] = true
+end
